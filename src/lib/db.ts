68756fc4b198b040d59fd3ae4bf63c818a50b553
Priyanshu <priyanshu911chaudhary@gmail.com>
2026-09-18 18:51:24 +0530

    chore: setup prisma 7 with postgreSQL and NeonDB and run a migration test

---
+++ src/lib/db.ts
@@ -0,0 +1,25 @@
+import {PrismaClient} from "@/generated/prisma/client"
+import {PrismaPg} from "@prisma/adapter-pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const adapter=new PrismaPg({
+    connectionString:process.env.DATABASE_URL
+})
+
+const prismaClientSingleton=()=>{
+    return new PrismaClient({
+        adapter
+    })
+}
+
+declare const globalThis:{
+    prismaGlobal:ReturnType<typeof prismaClientSingleton>
+}& typeof global
+
+export const prisma=globalThis.prismaGlobal || prismaClientSingleton()
+
+if(process.env.NODE_ENV!=='production') globalThis.prismaGlobal=prisma
+
+export default prisma
