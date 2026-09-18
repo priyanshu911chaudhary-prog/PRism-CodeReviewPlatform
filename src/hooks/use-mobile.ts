76524af280aaf2774d85a458bc2fa8e24451d19f
Priyanshu <priyanshu911chaudhary@gmail.com>
2026-09-18 17:36:05 +0530

    chore: initialize Nextjs with shadcn ui and setup a custom theme

---
+++ src/hooks/use-mobile.ts
@@ -0,0 +1,33 @@
+"use client"
+
+import * as React from "react"
+
+const MOBILE_BREAKPOINT = 768
+
+const subscribe = (callback: () => void) => {
+  const mql = window.matchMedia(
+    `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+  )
+
+  mql.addEventListener("change", callback)
+
+  return () => {
+    mql.removeEventListener("change", callback)
+  }
+}
+
+const getSnapshot = () => {
+  return window.innerWidth < MOBILE_BREAKPOINT
+}
+
+const getServerSnapshot = () => {
+  return false
+}
+
+export function useIsMobile() {
+  return React.useSyncExternalStore(
+    subscribe,
+    getSnapshot,
+    getServerSnapshot
+  )
+}
